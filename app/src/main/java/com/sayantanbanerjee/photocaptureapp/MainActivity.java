b73c93a2e5764a91edd6e858e2d9afe6f19eeab2
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2019-09-15 03:56:16 +0530

    Permission statements added.

--- app/src/main/java/com/sayantanbanerjee/photocaptureapp/MainActivity.java
+++ app/src/main/java/com/sayantanbanerjee/photocaptureapp/MainActivity.java
@@ -1,22 +1,25 @@
 package com.sayantanbanerjee.photocaptureapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView image;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
-        ImageView image = (ImageView) findViewById(R.id.splashImage);
+    public void function(){
         image.animate().alphaBy(1f).setDuration(1500);
-        new CountDownTimer(1500,1000){
+        new CountDownTimer(1500, 1000) {
 
             @Override
             public void onTick(long l) {
@@ -30,5 +33,40 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.finish();
             }
         }.start();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1 : {
+                if(grantResults.length >0 && grantResults[0]==PackageManager.PERMISSION_GRANTED && grantResults[1]==PackageManager.PERMISSION_GRANTED && grantResults[2]==PackageManager.PERMISSION_GRANTED ){
+                    function();
+                }else{
+                    Toast.makeText(this,"Permission denied...",Toast.LENGTH_SHORT).show();
+                    function();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        image = (ImageView) findViewById(R.id.splashImage);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if ((ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) || (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) || (ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)) {
+                String[] permission = {Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                ActivityCompat.requestPermissions(this,permission, 1);
+            }else{
+                function();
+            }
+        }else{
+            function();
+        }
+
     }
 }
--- app/src/main/java/com/sayantanbanerjee/photocaptureapp/fragments/HomePageFragment.java
+++ app/src/main/java/com/sayantanbanerjee/photocaptureapp/fragments/HomePageFragment.java
@@ -1,5 +1,8 @@
 package com.sayantanbanerjee.photocaptureapp.fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,9 +15,17 @@ import androidx.fragment.app.Fragment;
 import com.sayantanbanerjee.photocaptureapp.R;
 
 public class HomePageFragment extends Fragment {
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 }
