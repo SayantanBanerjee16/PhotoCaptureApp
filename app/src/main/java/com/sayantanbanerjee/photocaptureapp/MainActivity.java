ac390c9f07b1e39829cbbf2963762d8e0bda3f05
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2019-09-15 02:42:23 +0530

    Splash screen completed

--- app/src/main/java/com/sayantanbanerjee/photocaptureapp/MainActivity.java
+++ app/src/main/java/com/sayantanbanerjee/photocaptureapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.sayantanbanerjee.photocaptureapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("PHOTO PICKER APP");
+        getSupportActionBar().hide();
+        ImageView image = (ImageView) findViewById(R.id.splashImage);
+        image.animate().alphaBy(1f).setDuration(1500);
+        new CountDownTimer(1500,1000){
+
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(MainActivity.this, BottomNavigation_Activity.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+            }
+        }.start();
     }
 }
