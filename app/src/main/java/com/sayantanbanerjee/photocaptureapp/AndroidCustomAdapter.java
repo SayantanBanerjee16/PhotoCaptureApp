705a0cacfc4bcacba20eaa86a962aceba8abc4c2
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2019-09-16 01:42:23 +0530

    Bug & UI fixes

--- app/src/main/java/com/sayantanbanerjee/photocaptureapp/AndroidCustomAdapter.java
+++ app/src/main/java/com/sayantanbanerjee/photocaptureapp/AndroidCustomAdapter.java
@@ -37,72 +37,76 @@ public class AndroidCustomAdapter extends ArrayAdapter {
                     R.layout.list_item, parent, false);
         }
         String currentPhoto = (getItem(position)).toString();
+        String time = null,date = null;
+
+        try {
+            String day_string1 = Character.toString(currentPhoto.charAt(10)) + Character.toString(currentPhoto.charAt(11));
+            String month_string1 = Character.toString(currentPhoto.charAt(8)) + Character.toString(currentPhoto.charAt(9));
+            String year_string1 = Character.toString(currentPhoto.charAt(4)) + Character.toString(currentPhoto.charAt(5)) +
+                    Character.toString(currentPhoto.charAt(6)) + Character.toString(currentPhoto.charAt(7));
+
+            DAY_CURRENT = Integer.parseInt(day_string1);
+            MONTH_CURRENT = Integer.parseInt(month_string1);
+            YEAR_CURRENT = Integer.parseInt(year_string1);
+
+            String hour_string1 = Character.toString(currentPhoto.charAt(13)) + Character.toString(currentPhoto.charAt(14));
+            HOUR_CURRENT = Integer.parseInt(hour_string1);
+
+            String minute_string1 = Character.toString(currentPhoto.charAt(15)) + Character.toString(currentPhoto.charAt(16));
+            MINUTE_CURRENT = Integer.parseInt(minute_string1);
+
+            String second_string1 = Character.toString(currentPhoto.charAt(17)) + Character.toString(currentPhoto.charAt(18));
+            SECOND_CURRENT = Integer.parseInt(second_string1);
+
+            String minute;
+
+            if(MINUTE_CURRENT == 0){
+                minute = "00";
+            }else if(MINUTE_CURRENT < 10){
+                minute = "0" + MINUTE_CURRENT;
+            }else {
+                minute = Integer.toString(MINUTE_CURRENT);
+            }
 
 
-        String day_string1 = Character.toString(currentPhoto.charAt(10)) + Character.toString(currentPhoto.charAt(11));
-        String month_string1 = Character.toString(currentPhoto.charAt(8)) + Character.toString(currentPhoto.charAt(9));
-        String year_string1 = Character.toString(currentPhoto.charAt(4)) + Character.toString(currentPhoto.charAt(5)) +
-                Character.toString(currentPhoto.charAt(6)) + Character.toString(currentPhoto.charAt(7));
+            if (HOUR_CURRENT == 0) {
+                time = Integer.toString(12) + " : " + minute  + " AM";
+            } else if (HOUR_CURRENT < 12 && HOUR_CURRENT > 0) {
+                time = Integer.toString(HOUR_CURRENT) + " : " + minute  + " AM";
+            } else if (HOUR_CURRENT == 12)
+            {
+                time = Integer.toString(HOUR_CURRENT) + " : " + minute + " PM";
+            } else
+            {
+                time = Integer.toString(HOUR_CURRENT - 12) + " : " + minute + " PM";
+            }
 
-        DAY_CURRENT = Integer.parseInt(day_string1);
-        MONTH_CURRENT = Integer.parseInt(month_string1);
-        YEAR_CURRENT = Integer.parseInt(year_string1);
+            ArrayList<String> list = new ArrayList<String>();
+            list.add("Jan");
+            list.add("Feb");
+            list.add("Mar");
+            list.add("Apr");
+            list.add("May");
+            list.add("Jun");
+            list.add("Jul");
+            list.add("Aug");
+            list.add("Sept");
+            list.add("Oct");
+            list.add("Nov");
+            list.add("Dec");
 
-        String hour_string1 = Character.toString(currentPhoto.charAt(13)) + Character.toString(currentPhoto.charAt(14));
-        HOUR_CURRENT = Integer.parseInt(hour_string1);
+            date = list.get(MONTH_CURRENT - 1) + " " + Integer.toString(DAY_CURRENT) +", " + Integer.toString(YEAR_CURRENT);
 
-        String minute_string1 = Character.toString(currentPhoto.charAt(15)) + Character.toString(currentPhoto.charAt(16));
-        MINUTE_CURRENT = Integer.parseInt(minute_string1);
-
-        String second_string1 = Character.toString(currentPhoto.charAt(17)) + Character.toString(currentPhoto.charAt(18));
-        SECOND_CURRENT = Integer.parseInt(second_string1);
-
-        String minute;
-
-        if(MINUTE_CURRENT == 0){
-            minute = "00";
-        }else if(MINUTE_CURRENT < 10){
-            minute = "0" + MINUTE_CURRENT;
-        }else {
-            minute = Integer.toString(MINUTE_CURRENT);
+            TextView nameTextView = (TextView) listItemView.findViewById(R.id.photo_name);
+            nameTextView.setText("NAME : " + currentPhoto);
+            TextView dateTextView = (TextView) listItemView.findViewById(R.id.photo_date);
+            dateTextView.setText("DATE : " + date);
+            TextView timeTextView = (TextView) listItemView.findViewById(R.id.photo_time);
+            timeTextView.setText("TIME : " + time);
+        }catch (Exception e){
+            e.printStackTrace();
         }
 
-        String time,date;
-        if (HOUR_CURRENT == 0) {
-            time = Integer.toString(12) + " : " + minute  + " AM";
-        } else if (HOUR_CURRENT < 12 && HOUR_CURRENT > 0) {
-            time = Integer.toString(HOUR_CURRENT) + " : " + minute  + " AM";
-        } else if (HOUR_CURRENT == 12)
-        {
-            time = Integer.toString(HOUR_CURRENT) + " : " + minute + " PM";
-        } else
-        {
-            time = Integer.toString(HOUR_CURRENT - 12) + " : " + minute + " PM";
-        }
-
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("Jan");
-        list.add("Feb");
-        list.add("Mar");
-        list.add("Apr");
-        list.add("May");
-        list.add("Jun");
-        list.add("Jul");
-        list.add("Aug");
-        list.add("Sept");
-        list.add("Oct");
-        list.add("Nov");
-        list.add("Dec");
-
-        date = list.get(MONTH_CURRENT - 1) + " " + Integer.toString(DAY_CURRENT) +", " + Integer.toString(YEAR_CURRENT);
-
-
-        TextView nameTextView = (TextView) listItemView.findViewById(R.id.photo_name);
-        nameTextView.setText("NAME : " + currentPhoto);
-        TextView dateTextView = (TextView) listItemView.findViewById(R.id.photo_date);
-        dateTextView.setText("DATE : " + date);
-        TextView timeTextView = (TextView) listItemView.findViewById(R.id.photo_time);
-        timeTextView.setText("TIME : " + time);
         try {
             ImageView smallImage = (ImageView) listItemView.findViewById(R.id.list_item_icon);
             String path = Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY + "/" + currentPhoto;
--- app/src/main/java/com/sayantanbanerjee/photocaptureapp/PicActivity.java
+++ app/src/main/java/com/sayantanbanerjee/photocaptureapp/PicActivity.java
@@ -6,17 +6,34 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.io.File;
 
 public class PicActivity extends AppCompatActivity {
     ImageView bigImageView;
+    ProgressBar progressBar;
+    TextView textView;
+
+    @Override
+    public void onBackPressed() {
+        bigImageView.setAlpha(0.2f);
+        textView.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+        super.onBackPressed();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pic);
+        textView = findViewById(R.id.loading_view6);
+        progressBar = findViewById(R.id.loading_view7);
+        textView.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.INVISIBLE);
         Intent intent = getIntent();
         String path = intent.getStringExtra("path");
         bigImageView = (ImageView) findViewById(R.id.imageBig);
