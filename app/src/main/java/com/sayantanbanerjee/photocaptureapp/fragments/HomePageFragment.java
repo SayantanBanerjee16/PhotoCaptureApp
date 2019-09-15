17b42e8d365c41c4b9386db1b526fb57789b2207
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2019-09-15 11:12:10 +0530

    Capture Photo unctionality implemented. Blurred Image

--- app/src/main/java/com/sayantanbanerjee/photocaptureapp/fragments/HomePageFragment.java
+++ app/src/main/java/com/sayantanbanerjee/photocaptureapp/fragments/HomePageFragment.java
@@ -1,12 +1,19 @@
 package com.sayantanbanerjee.photocaptureapp.fragments;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,13 +25,75 @@ import androidx.fragment.app.Fragment;
 import com.sayantanbanerjee.photocaptureapp.MainActivity;
 import com.sayantanbanerjee.photocaptureapp.R;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.Calendar;
+
+import static android.media.MediaRecorder.VideoSource.CAMERA;
 public class HomePageFragment extends Fragment {
+
+    ImageView imageView;
+    private static final String IMAGE_DIRECTORY = "/PhotoCaptureApp";
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        Button button = (Button) view.findViewById(R.id.captureButton);
+        imageView = view.findViewById(R.id.photoPlaceholder);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA);
+            }
+        });
+        return view;
+    }
+
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        File wallpaperDirectory = new File(
+                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
+        if (!wallpaperDirectory.exists()) {
+            wallpaperDirectory.mkdirs();
+        }
+
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(getActivity(),
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::---&gt;" + f.getAbsolutePath());
+
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA) {
+            Bitmap currentImage = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(currentImage);
+            saveImage(currentImage);
+            Toast.makeText(getActivity(), "Image Saved!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
