a2451612957f1c1ed73117591011700de0e519e9
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2019-09-15 16:02:32 +0530

    Number of total photos captured are set

--- app/src/main/java/com/sayantanbanerjee/photocaptureapp/fragments/HomePageFragment.java
+++ app/src/main/java/com/sayantanbanerjee/photocaptureapp/fragments/HomePageFragment.java
@@ -1,7 +1,9 @@
 package com.sayantanbanerjee.photocaptureapp.fragments;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -19,6 +21,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -50,19 +53,24 @@ import static android.os.Environment.getExternalStoragePublicDirectory;
 public class HomePageFragment extends Fragment {
 
     ImageView imageView;
+    TextView textView;
     private static final String IMAGE_DIRECTORY = "/PhotoCaptureApp";
     private String pictureImagePath = "";
+
+    public int VAL = 0;
+    SharedPreferences sharedPreferences;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
         Button button = (Button) view.findViewById(R.id.captureButton);
         imageView = view.findViewById(R.id.photoPlaceholder);
-        button.setOnClickListener(new View.OnClickListener()
-        {
+        textView = view.findViewById(R.id.footer);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
                 String imageFileName = timeStamp + ".jpg";
                 File storageDir = Environment.getExternalStoragePublicDirectory(
@@ -80,7 +88,6 @@ public class HomePageFragment extends Fragment {
     }
 
 
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -88,41 +95,41 @@ public class HomePageFragment extends Fragment {
             if (resultCode == RESULT_OK) {
                 File imgFile = new File(pictureImagePath);
                 Bitmap bitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-                try{
-                ExifInterface exif = new ExifInterface(imgFile.getPath());
-                int orientation = exif.getAttributeInt(
-                        ExifInterface.TAG_ORIENTATION,
-                        ExifInterface.ORIENTATION_NORMAL);
+                try {
+                    ExifInterface exif = new ExifInterface(imgFile.getPath());
+                    int orientation = exif.getAttributeInt(
+                            ExifInterface.TAG_ORIENTATION,
+                            ExifInterface.ORIENTATION_NORMAL);
 
-                int angle = 0;
+                    int angle = 0;
 
-                if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
-                    angle = 90;
-                } else if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
-                    angle = 180;
-                } else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
-                    angle = 270;
+                    if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
+                        angle = 90;
+                    } else if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
+                        angle = 180;
+                    } else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+                        angle = 270;
+                    }
+
+                    Matrix mat = new Matrix();
+                    mat.postRotate(angle);
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inSampleSize = 2;
+
+                    Bitmap bmp = BitmapFactory.decodeStream(new FileInputStream(imgFile),
+                            null, options);
+                    bitmap = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(),
+                            bmp.getHeight(), mat, true);
+                    ByteArrayOutputStream outstudentstreamOutputStream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100,
+                            outstudentstreamOutputStream);
+                    imageView.setImageBitmap(bitmap);
+
+                } catch (IOException e) {
+                    Log.w("TAG", "-- Error in setting image");
+                } catch (OutOfMemoryError oom) {
+                    Log.w("TAG", "-- OOM Error in setting image");
                 }
-
-                Matrix mat = new Matrix();
-                mat.postRotate(angle);
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inSampleSize = 2;
-
-                Bitmap bmp = BitmapFactory.decodeStream(new FileInputStream(imgFile),
-                        null, options);
-                bitmap = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(),
-                        bmp.getHeight(), mat, true);
-                ByteArrayOutputStream outstudentstreamOutputStream = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100,
-                        outstudentstreamOutputStream);
-                imageView.setImageBitmap(bitmap);
-
-            } catch (IOException e) {
-                Log.w("TAG", "-- Error in setting image");
-            } catch (OutOfMemoryError oom) {
-                Log.w("TAG", "-- OOM Error in setting image");
-            }
                 if (imgFile.exists()) {
 
                     imageView.setImageBitmap(bitmap);
@@ -140,10 +147,12 @@ public class HomePageFragment extends Fragment {
         if (!wallpaperDirectory.exists()) {
             wallpaperDirectory.mkdirs();
         }
+        // Calendar.getInstance().getTimeInMillis()
 
         try {
-            File f = new File(wallpaperDirectory, Calendar.getInstance()
-                    .getTimeInMillis() + ".jpg");
+            int val = sharedPreferences.getInt("firstTime", 0);
+            sharedPreferences.edit().putInt("firstTime", val + 1).apply();
+            File f = new File(wallpaperDirectory, "IMG" + val + ".jpg");
             f.createNewFile();
             FileOutputStream fo = new FileOutputStream(f);
             fo.write(bytes.toByteArray());
@@ -152,6 +161,7 @@ public class HomePageFragment extends Fragment {
                     new String[]{"image/jpeg"}, null);
             fo.close();
             Log.d("TAG", "File Saved::---&gt;" + f.getAbsolutePath());
+            textView.setText("No of PICTURES total captured by this App are: " + Integer.toString(val - 1));
 
             return f.getAbsolutePath();
         } catch (IOException e1) {
@@ -163,11 +173,12 @@ public class HomePageFragment extends Fragment {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case 1 : {
-                if(grantResults.length >0 && grantResults[0]==PackageManager.PERMISSION_GRANTED && grantResults[1]==PackageManager.PERMISSION_GRANTED && grantResults[2]==PackageManager.PERMISSION_GRANTED ){
-                }else{
-                    Toast.makeText(getActivity(),"Permission denied...",Toast.LENGTH_SHORT).show();
+        textView.setAlpha(0);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED && grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(getActivity(), "Permission denied...", Toast.LENGTH_SHORT).show();
                 }
             }
         }
@@ -179,13 +190,33 @@ public class HomePageFragment extends Fragment {
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
+    public void onStart() {
+        super.onStart();
+        textView.setAlpha(0);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if ((ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) || (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) || (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)) {
-                String[] permission = {Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                ActivityCompat.requestPermissions(getActivity(),permission, 1);
+            if ((ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) || (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) || (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)) {
+                String[] permission = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                ActivityCompat.requestPermissions(getActivity(), permission, 1);
             }
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sharedPreferences = getActivity().getSharedPreferences("com.sayantanbanerjee.photocaptureapp", Context.MODE_PRIVATE);
+        int firstTime = sharedPreferences.getInt("firstTime", 0);
+        int firstTime2 = firstTime;
+        if (firstTime2 == 0) {
+            sharedPreferences.edit().putInt("firstTime", 1).apply();
+        }
+        textView.setAlpha(1);
+        if (firstTime2 == 0) {
+            textView.setText("No of PICTURES total captured by this App are: " + 0);
+        } else {
+            textView.setText("No of PICTURES total captured by this App are: " + Integer.toString(firstTime2 - 1));
+        }
+
+
     }
 }
